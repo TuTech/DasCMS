6b195c41b0ed7250946f4fdbfdc4f630fc546cba
Lutz Selke <selke@tutech.de>
2008-11-19 16:46:48 +0000

    cache control headers for scheduler

--- scheduler.php
+++ scheduler.php
@@ -1,6 +1,9 @@
 <?php
 require_once('./System/Component/Loader.php');
 header('Content-Type: image/png;');
+header("Expires: ".date('r', 0));
+header("Cache-Control: max-age=0, public");
+
 if(SJobScheduler::runJob())
 {
     readfile(SPath::SYSTEM_IMAGES.'ok.png');
