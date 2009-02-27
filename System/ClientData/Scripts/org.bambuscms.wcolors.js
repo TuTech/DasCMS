8d86a9cb33b7b09026ab061ec8ee13c4d0a97ea9
Lutz Selke <selke@tutech.de>
2009-02-27 09:42:38 +0000

    optimized css color palette

--- System/ClientData/Scripts/org.bambuscms.wcolors.js
+++ System/ClientData/Scripts/org.bambuscms.wcolors.js
@@ -2,27 +2,60 @@ org.bambuscms.wcolors = {};
 org.bambuscms.wcolors.interval = null;
 org.bambuscms.wcolors.intervalTime = 2000;
 org.bambuscms.wcolors.run = function(){
-	//org.bambuscms.wnotifications.report(org.bambuscms.wnotifications.INFORMATION, 'running');
 	var text = $(org.bambuscms.app.document.editorElementId).value;
-	var regexp = /#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})[^a-fA-F0-9]/g;//(#([a-zA-Z0-9]{3}|[a-zA-Z0-9]{6}))[\s,;]+/g;
-	var res;// = text.match(regexp);
+	var regexp = /#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})[^a-fA-F0-9]/g;
+	var res, c;
 	var colors = {};
-	var no;
 	while((res = regexp.exec(text)) != null)
 	{
-		colors[res[1]] = (colors[res[1]] >= 1) ? (colors[res[1]]+1) : 1;
+		c = res[1].toUpperCase();
+		if(colors[c])
+		{
+			colors[c].used++;
+		}
+		else
+		{
+			if(c.length == 3)
+			{
+				r = parseInt(c.substr(0,1)+c.substr(0,1),16);
+				g = parseInt(c.substr(1,1)+c.substr(1,1),16);
+				b = parseInt(c.substr(2,1)+c.substr(2,1),16);
+			}
+			else
+			{
+				r = parseInt(c.substr(0,2),16);
+				g = parseInt(c.substr(2,2),16);
+				b = parseInt(c.substr(4,2),16);
+			}
+			colors[c] = {
+				'used':1,
+				'r':r,
+				'g':g,
+				'b':b,
+				'l':(r+g+b),
+				'hex': c,
+			};
+		}
 	}
 	var out = '';
 	var textColor;
-	var min = '888888';
+	var sorted = [];
 	for(color in colors)
 	{
-		textColor = (parseInt(color, 16) < parseInt(min.substr(0,color.length),16)) ? 'WColors-light' : 'WColors-dark';
-		out += '<div style="background:#'+color+';" class="'+textColor+'" onclick="org.bambuscms.app.document.insertText(\'#'+color+'\');">#'+color+' ('+colors[color]+')</div>\n';
+		sorted[sorted.length] = colors[color];
+	}
+	sorted.sort(org.bambuscms.wcolors.lumiSort);
+	for(var i = 0; i < sorted.length; i++)
+	{
+		color = sorted[i];
+		cssClass = (color.l < 192) ? 'WColors-light' : 'WColors-dark';
+		out += '<div style="background:#'+color.hex+';" class="'+cssClass+'" onclick="org.bambuscms.app.document.insertText(\'#'+color.hex+'\');">'+
+				'#'+color.hex+' ('+color.used+')</div>\n';
 	}
 	$('WColors-area').innerHTML = out;
-	//org.bambuscms.wnotifications.report(org.bambuscms.wnotifications.INFORMATION, res[1]);
-
+};
+org.bambuscms.wcolors.lumiSort = function(a, b){
+	return b.l - a.l;
 };
 org.bambuscms.wcolors.load = function(){
 	if($(org.bambuscms.app.document.editorElementId) && $('WColors-area'))
