640882689ff93fcabe59877c80eaa0f6b6dd74f4
Lutz Selke <selke@tutech.de>
2009-03-19 15:59:46 +0000

    started javascript MVC-based person editor

--- System/Applications/Persons.bap/edit.php
+++ System/Applications/Persons.bap/edit.php
@@ -14,3 +14,30 @@ if(isset($User) && $User instanceof CPerson)
     	);
 }
 ?>
+<h3>Tel.:</h3>
+<div class="PersonAttributeBlock">
+	<select class="PersonContextSelect">
+		<optgroup label="Contexts">
+			<option>Arbeit</option>
+			<option>Privat</option>
+		</optgroup>
+		<optgroup label="Aktionen">
+			<option>Eigenschaft entfernen</option>
+			<option>Neuer Context</option>
+			<option>Ungenutzte Contexte entfernen</option>
+		</optgroup>
+	</select>
+	<input type="text" value="+49 15464 35 345 32">
+	<br />
+	<br />
+	<select class="">
+		<option>Eigenschaft hinzufügen</option>
+		<optgroup label="Contexts">
+			<option>Arbeit</option>
+			<option>Privat</option>
+		</optgroup>
+	</select>
+</div>
+<div id="org_bambuscms_app_persons_gui">
+
+</div>
--- System/Applications/Persons.bap/script.js
+++ System/Applications/Persons.bap/script.js
@@ -19,4 +19,103 @@ function Delete()
 	org.bambuscms.app.dialog.create(_('delete_template'), _('do_you_really_want_to_delete_this_text'), input, _('yes'), _('no'));
 	org.bambuscms.app.dialog.setAction('delete');
 }
+org.bambuscms.app.persons = {};
+org.bambuscms.app.persons.attributes = {
+	'phone':{
+		'contexts': ['arbeit', 'privat','mobil','fax arbeit', 'fax privat'],
+		'type':'phone',
+		'values':{
+			'arbeit':'+49 01054 2540 4521',
+			'privat':'23904 555-7530'
+		}
+	},
+	'im':{
+		'contexts': ['aim', 'jabber','icq'],
+		'type':'text',
+		'values':{
+			'aim':'dingsda',
+			'jabber':'bumsta@foo.da'
+		}
+	}
+};
 
+org.bambuscms.app.persons.controller = {
+	'shown':{},
+	'addAttribute':function(attribute, type)
+	{
+		if(!org.bambuscms.app.persons.controller.shown[attribute])
+		{
+			org.bambuscms.app.persons.attributes[attribute] = {
+				'contexts':[],
+				'type':type,
+				'values':{}
+			};
+			org.bambuscms.app.persons.controller.shown[attribute] = 1;
+			//add to gui
+			org.bambuscms.app.persons.gui.createAttribute(attribute, type);
+		}
+	},
+	'addContext':function(attribute, context)
+	{
+		if(!this.attributes[attribute])
+		{
+			for(var i = 0; i < this.attributes[attribute].contexts; i++)
+			{
+				if(this.attributes[attribute].contexts == context)
+				{
+					return;
+				}
+			}
+			this.attributes[attribute].contexts[this.attributes[attribute].contexts.length] = context;
+			this.attributes[attribute].contexts.sort();
+		}
+	},
+	'removeContext':function(attribute, context)
+	{
+		
+	}
+};
+org.bambuscms.app.persons.gui = {
+	'createAttribute':function(attribute, type)
+	{
+		var els = $('org_bambuscms_app_persons_gui').getElementsByTagName('div');
+		var insert = org.bambuscms.app.persons.gui.attributeHTML(attribute, type);
+		if(els.length > 0)
+		{
+			var i = 0;
+			while(els[i].nodeValue < attribute)
+			{
+				i++;
+			}
+			if(!els[i].nextSibling)
+			{
+				$('org_bambuscms_app_persons_gui').appendChild(insert);
+			}
+			else
+			{
+				$('org_bambuscms_app_persons_gui').insertBefore(insert,els[i].nextSibling)
+			}
+		}
+		else
+		{
+			$('org_bambuscms_app_persons_gui').appendChild(insert);
+		}
+	},
+	'attributeHTML':function(attribute, type)
+	{
+		var box = document.createElement('div');
+		box.className = 'PersonAttributeBlock';
+		var h3 = document.createElement('h3');
+		h3.appendChild(document.createTextNode(attribute));
+		box.appendChild(h3);
+		return box;
+	}
+};
+(function(){
+	org.bambuscms.autorun.register(function(){
+		for(att in org.bambuscms.app.persons.attributes)
+		{
+			org.bambuscms.app.persons.controller.addAttribute(att, org.bambuscms.app.persons.attributes[att].type);
+		}
+	});
+})();
