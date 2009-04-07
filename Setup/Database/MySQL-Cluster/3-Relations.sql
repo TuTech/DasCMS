5e3bbc51ca2a1d0f55bd82c83119bae136b7ed91
Lutz Selke <selke@tutech.de>
2009-04-07 07:02:35 +0000

    removed mysql-cluster sql files - cluster does not support foreign keys

--- Setup/Database/MySQL-Cluster/3-Relations.sql
+++
@@ -1,9 +0,0 @@
- CREATE TABLE IF NOT EXISTS relContentsPreviewImages( contentREL INTEGER UNIQUE NOT NULL, previewREL INTEGER NOT NULL, INDEX (previewREL) )  CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
- CREATE TABLE IF NOT EXISTS relContentsTags( contentREL INTEGER NOT NULL, tagREL INTEGER NOT NULL, INDEX (contentREL), INDEX (tagREL) )  CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
- CREATE TABLE IF NOT EXISTS relFeedsContents( feedREL INTEGER NOT NULL, contentREL INTEGER NOT NULL, INDEX (feedREL), INDEX (contentREL) )  CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
- CREATE TABLE IF NOT EXISTS relFeedsTags( feedREL INTEGER NOT NULL, tagREL INTEGER NOT NULL, INDEX (feedREL), INDEX (tagREL) )  CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
- CREATE TABLE IF NOT EXISTS relMailImportAccountsMailImportFlags( mailImportAccountREL INTEGER NOT NULL, mailImportFlagREL INTEGER NOT NULL )  CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
- CREATE TABLE IF NOT EXISTS relPermissionTagsGroups( permissionTagREL INTEGER NOT NULL, groupREL INTEGER NOT NULL, INDEX (permissionTagREL), UNIQUE (groupREL, permissionTagREL) )  CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
- CREATE TABLE IF NOT EXISTS relPermissionTagsUsers( permissionTagREL INTEGER NOT NULL, userREL INTEGER NOT NULL, INDEX (permissionTagREL), UNIQUE (userREL, permissionTagREL) )  CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
- CREATE TABLE IF NOT EXISTS relUsersGroups( userREL INTEGER NOT NULL, groupREL INTEGER NOT NULL, INDEX (userREL), UNIQUE (groupREL, userREL) )  CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
- 
