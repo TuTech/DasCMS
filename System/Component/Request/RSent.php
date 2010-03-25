8731aa71f59fe96dbd677e405f01a636b91b3f31
Lutz Selke <selke@tutech.de>
2010-03-25 12:19:43 +0000

    support for array data in get/post more info for database exceptions

--- System/Component/Request/RSent.php
+++ System/Component/Request/RSent.php
@@ -9,34 +9,25 @@
  * @package Bambus
  * @subpackage Request
  */
-class RSent extends BRequest 
+class RSent extends BRequest
 {
     //read data from server path
     //merge data with $_POST
 
     private static $data = null;
-    
+
     private function __construct(){}
-    
+
     private static function init()
     {
         global $_POST;
         if(self::$data == null)
         {
             self::$data = $_POST;
-            if(get_magic_quotes_gpc())
-            {
-                foreach ($_POST as $key => $value) 
-                {
-                    if(!is_array($value))
-                    {
-                        self::$data[$key] = stripslashes($value);
-                    }
-                }
-            }
+            BRequest::cleanGPC(self::$data);
         }
     }
-    
+
     public static function get($key, $encoding = "ISO-8859-1")
     {
         self::init();
@@ -45,49 +36,36 @@ class RSent extends BRequest
         {
             $ret = self::$data[$key];
         }
-        if($encoding == "ISO-8859-1")
-        {
-            return utf8_decode($ret);
-        }
-        if(strtolower($encoding) == CHARSET)
-        {
-            return $ret;
-        }
-        return mb_convert_encoding($ret, $encoding, CHARSET);
+        return BRequest::recodeCharset($ret, $encoding);
     }
-    
+
     public static function has($key)
     {
         self::init();
         return array_key_exists($key, self::$data);
     }
-    
+
     public static function hasValue($key)
     {
         self::init();
         return (array_key_exists($key, self::$data) && !empty(self::$data[$key]));
     }
-    
+
     public static function alter($key, $value, $encoding = "ISO-8859-15")
     {
         self::init();
         self::$data[$key] = mb_convert_encoding($value, CHARSET, $encoding);
     }
-    
-    public static function data($encoding = "ISO-8859-15")
+
+ 	public static function data($encoding = "ISO-8859-15")
     {
         self::init();
         $data = self::$data;
         if(strtoupper($encoding) != CHARSET)
         {
-            $data = array();
-            foreach (self::$data as $k => $v) 
-            {
-                $data[mb_convert_encoding($k, $encoding, CHARSET)] = mb_convert_encoding($v, $encoding, CHARSET);
-            }
-            
+            $data = BRequest::recodeCharset($data, $encoding);
         }
-        return self::$data;
+        return $data;
     }
 }
 ?>
--- System/Component/System/SErrorAndExceptionHandler.php
+++ System/Component/System/SErrorAndExceptionHandler.php
@@ -10,21 +10,21 @@
  * @subpackage System
  */
 class SErrorAndExceptionHandler
-    extends 
-        BSystem 
+    extends
+        BSystem
     implements
         HRequestingClassSettingsEventHandler,
         HUpdateClassSettingsEventHandler
 {
     private static $error = null;
     private static $errorMessage = null;
-    
+
     private static $report = true;
     private static $reportSkipOnce = false;
-    
+
     private static $showInfoMessage = false;
     private static $hideErrors = false;
-    private static $err_html = 
+    private static $err_html =
         '<div style="font-family:sans-serif;border:1px solid #a40000;">
             <div style="border:1px solid #cc0000;z-index:1000000;padding:10px;background:#a40000;color:white;">
                 <h1 style="border-bottom:1px solid #cc0000;font-size:16px;">%s <code>%d</code> in "%s" at line %d</h1>
@@ -33,10 +33,10 @@ class SErrorAndExceptionHandler
     			<p>CWD: %s</p>
             </div>
         </div>';
-    
-    private static $err_mail = 
+
+    private static $err_mail =
     	"\r\n\r\n%s (%d) in \"%s\" at line %d\n\n%s\n\n%s\n\ncwd: %s\nscript: %s\nurl: %s";
-    
+
     public function HandleRequestingClassSettingsEvent(ERequestingClassSettingsEvent $e)
     {
         $data = array(
@@ -46,7 +46,7 @@ class SErrorAndExceptionHandler
         );
         $e->addClassSettings($this, 'error_handling', $data);
     }
-    
+
     public function HandleUpdateClassSettingsEvent(EUpdateClassSettingsEvent $e)
     {
         $data = $e->getClassSettings($this);
@@ -63,7 +63,7 @@ class SErrorAndExceptionHandler
             LConfiguration::set('show_errors_on_website', $data['show_errors_on_website']);
         }
     }
-    
+
     private static function mail($kind, $code, $file, $line, $message, $stack, $workingDir)
     {
         if(LConfiguration::get('mail_webmaster_on_error') == '1')
@@ -89,105 +89,102 @@ class SErrorAndExceptionHandler
             }
         }
     }
-    
+
     public static function reportErrors()
     {
         self::$report = true;
     }
-    
+
     public static function showMessageBeforeDying()
     {
         self::$showInfoMessage = true;
     }
-    
+
     public static function hideErrors()
     {
         self::$hideErrors = true;
     }
-    
+
     public static function muteErrors()
     {
         self::$report = false;
     }
-    
+
     public static function muteErrorOnce()
     {
         self::$reportSkipOnce = true;
     }
-    
+
     public static function getLastError()
     {
         return self::$error;
     }
-    
+
     public static function getLastErrorMessage()
     {
         return self::$errorMessage;
     }
-    
+
     public static function errorHandler($errno, $errstr, $errfile, $errline, $errcontext)
     {
-        try
+        ob_start();
+        print_r($errcontext);
+        $context = ob_get_contents();
+        ob_end_clean();
+        $err = sprintf(
+            self::$err_html
+            , 'Error'
+            , $errno
+            , $errfile
+            , $errline
+            , $errstr
+            , $context
+            ,getcwd());
+        DFileSystem::Append(SPath::LOGS().'Error.log', $err);
+        self::$error = array($errno, $errstr, $errfile, $errline, $errcontext);
+        self::$errorMessage = $err;
+        if(self::$report && !self::$reportSkipOnce)
         {
-            ob_start();
-            print_r($errcontext);
-            $context = ob_get_contents();
-            ob_end_clean();
-            $err = sprintf(
-                self::$err_html
-                , 'Error'
+            self::mail(
+            	'Error'
                 , $errno
                 , $errfile
                 , $errline
                 , $errstr
                 , $context
                 ,getcwd());
-            DFileSystem::Append(SPath::LOGS.'Error.log', $err);
-            self::$error = array($errno, $errstr, $errfile, $errline, $errcontext);
-            self::$errorMessage = $err;
-            if(self::$report && !self::$reportSkipOnce)
-            {
-                self::mail(
-                	'Error'
+            SNotificationCenter::report(
+            	'warning',
+                sprintf(
+                	'%s %d in %s at %s: %s'
+                    , 'Error'
                     , $errno
                     , $errfile
                     , $errline
                     , $errstr
                     , $context
-                    ,getcwd());
-                SNotificationCenter::report(
-                	'warning',
-                    sprintf(
-                    	'%s %d in %s at %s: %s'
-                        , 'Error'
-                        , $errno
-                        , $errfile
-                        , $errline
-                        , $errstr
-                        , $context
-                        ,getcwd()));
-                if(!self::$hideErrors)
-                {
-                    echo $err;
-                }
+                    ,getcwd()));
+            if(!self::$hideErrors)
+            {
+                echo $err;
             }
-            self::$reportSkipOnce = false;
         }
-        catch(Exception $e)
-        {
-            echo 'EXCEPTION IN ERROR HANDLER '.$e."<br />\n";
-        }
+        self::$reportSkipOnce = false;
     }
-    
+
     public static function reportException(Exception $e)
     {
+    	$debugInfo = ($e instanceof XDatabaseException)
+    		? $e->getSQL()."\n\n"
+    		: '';
+    	$debugInfo .= $e->getTraceAsString();
         self::mail(
             get_class($e)
             , $e->getCode()
             , $e->getFile()
             , $e->getLine()
             , $e->getMessage()
-            , $e->getTraceAsString()
+            , $debugInfo
             ,getcwd());
         $err = sprintf(
             self::$err_html
@@ -196,33 +193,26 @@ class SErrorAndExceptionHandler
             , $e->getFile()
             , $e->getLine()
             , $e->getMessage()
-            , $e->getTraceAsString()
+            , $debugInfo
             ,getcwd());
-        DFileSystem::Append(SPath::LOGS.'Exceptions.log', $err);
-        return $err;    
+        DFileSystem::Append(SPath::LOGS().'Exceptions.log', $err);
+        return $err;
     }
-            
+
     public static function exceptionHandler(Exception $e)
     {
-        try
+        $str = self::reportException($e);
+        if(!self::$hideErrors)
         {
-            $str = self::reportException($e);
-            if(!self::$hideErrors)
-            {
-                echo $str;
-            }
-            if(self::$showInfoMessage)
-            {
-                $f = LConfiguration::get('error_info_text_file');
-                if(!empty($f) && file_exists($f) && is_readable($f) && substr(basename($f),0,1) != '.')
-                {
-                    readfile($f);
-                }
-            }
+            echo $str;
         }
-        catch(Exception $e)
+        if(self::$showInfoMessage)
         {
-            echo 'EXCEPTION IN EXCEPTION HANDLER '.$e."<br />\n";
+            $f = LConfiguration::get('error_info_text_file');
+            if(!empty($f) && file_exists($f) && is_readable($f) && substr(basename($f),0,1) != '.')
+            {
+                readfile($f);
+            }
         }
         exit(1);
     }
