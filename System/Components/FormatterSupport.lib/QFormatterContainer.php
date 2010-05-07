34e339942f807a0cc3b39d4d34fb45c09c831b13
Lutz Selke <selke@tutech.de>
2010-05-07 15:00:52 +0000

    bugfix for formatter segfault

--- System/Components/FormatterSupport.lib/QFormatterContainer.php
+++ System/Components/FormatterSupport.lib/QFormatterContainer.php
@@ -14,7 +14,7 @@ class QFormatterContainer extends BQuery
     /**
      * result has exactly one row
      * @param string $name
-     * @return DSQLResult 
+     * @return DSQLResult
      */
     public static function getFormatter($name)
     {
@@ -24,19 +24,38 @@ class QFormatterContainer extends BQuery
         $res = $DB->query($sql, DSQL::NUM);
         if($res->getRowCount() != 1)
         {
+			$res->free();
             throw new XUndefinedIndexException('formatter not found');
         }
         return $res;
     }
+	/**
+     * result has exactly one row
+     * @param string $name
+     * @return DSQLResult
+     */
+    public static function isFormatter($name)
+    {
+        $sql = "SELECT name FROM Formatters WHERE name = '%s'";
+        $DB = BQuery::Database();
+        $sql = sprintf($sql, $DB->escape($name));
+        $res = $DB->query($sql, DSQL::NUM);
+        $count = $res->getRowCount();
+		$res->free();
+        return $count;
+    }
         
     public static function setFormatter($name, $data)
     {
-        $sql = "INSERT INTO Formatters (name, formatterData) 
-        			VALUES ('%s', '%s')
-        			ON DUPLICATE KEY UPDATE formatterData = '%s'";
+		if(QFormatterContainer::isFormatter($name)){
+			$sql = "UPDATE Formatters SET formatterData = '%s' WHERE name = '%s'";
+		}
+		else{
+			$sql = "INSERT INTO Formatters (formatterData, name) VALUES ('%s', '%s')";
+		}
+
         $DB = BQuery::Database();
-        $data = $DB->escape($data);
-        $sql = sprintf($sql, $DB->escape($name), $data, $data);
+        $sql = sprintf($sql, $DB->escape($data), $DB->escape($name));
         return $DB->queryExecute($sql);
     }
         
