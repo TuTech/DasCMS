49895c9b44d2f3c0d2828311bc6bc88739961333
Lutz Selke <selke@tutech.de>
2010-05-12 08:40:03 +0000

    content view for assigned relations

---
+++ System/Components/ContentSupport.lib/View/Content/AssignedRelations.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Description of View_Content_AssignedRelations
+ *
+ * @author lse
+ */
+class View_Content_AssignedRelations
+	extends
+		_View_Content_Base
+	implements
+		Interface_View_DisplayXHTML,
+		Interface_View_Content
+{
+	public function toXHTML() {
+		$val = '';
+		if($this->shouldDisplay() && $this->content->hasComposite('AssignedRelations')){
+			$val = $this->wrapXHTML('AssignedRelations', $this->content->AssignedRelations);
+		}
+		return $val;
+	}
+}
+?>
