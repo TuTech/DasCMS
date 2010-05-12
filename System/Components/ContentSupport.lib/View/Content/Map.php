641e18140fe2f9fc0ac688f050c5fde597442bdd
Lutz Selke <selke@tutech.de>
2010-05-12 14:21:33 +0000

    google maps formatter

--- System/Components/ContentSupport.lib/View/Content/Map.php
+++ System/Components/ContentSupport.lib/View/Content/Map.php
@@ -1,10 +1,100 @@
 <?php
 /**
- * Description of Map
+ * Description of Content
  *
  * @author lse
  */
-class Map {
-    //put your code here
+class View_Content_Map
+	extends
+		_View_Content_Base
+	implements
+		Interface_View_DisplayXHTML,
+		Interface_View_Content
+{
+	//http://maps.google.com/staticmap?center=40.714728,-73.998672&zoom=14&size=512x512&maptype=mobile\
+	//&markers=40.702147,-74.015794
+	//&key=MAPS_API_KEY&sensor=false
+
+	protected $mapWidth = 100,
+			  $mapHeight = 100,
+			  $zoom = 13;
+
+	public function toXHTML() {
+		$val = '';
+		if(Core::classExists('UGoogleServices')
+				&& $this->content->hasComposite('Location')
+				&& LConfiguration::get('google_maps_key') != ''
+				&& $this->shouldDisplay()
+		){
+			$location = $this->content->Location;
+			if($location instanceof WContentGeoAttribute){
+				$long = $location->getLongitude();
+				$lat  = $location->getLatitude();
+
+				if($lat != '' && $long != ''){
+					$poi = sprintf('%f,%f', $lat, $long);
+					$map = '<img src="http://maps.google.com/staticmap?%s" alt="Map" style="width:%dpx;height:%dpx" />';
+					$urldata = array(
+						'center' => $poi,
+						'zoom'   => $this->zoom,
+						'size'   => sprintf('%dx%d', $this->mapWidth, $this->mapHeight),
+						'maptype'=> 'mobile',
+						'markers'=> $poi,
+						'key' => LConfiguration::get('google_maps_key'),
+						'sensor' => 'false'
+					);
+					$parts = array();
+					foreach ($urldata as $key => $value){
+						$parts[] = sprintf('%s=%s', urlencode($key), urlencode($value));
+					}
+
+					$map = sprintf($map, implode('&', $parts), $this->mapWidth, $this->mapHeight);
+
+					$val = $this->wrapXHTML('Map', $map);
+				}
+			}
+		}
+		return $val;
+	}
+	protected function getPersistentAttributes() {
+		return array(
+			'mapWidth',
+			'mapHeight',
+			'zoom'
+		);
+	}
+
+	public function getMapWidth(){
+		return $this->mapWidth;
+	}
+
+	public function setMapWidth($value){
+		if(!is_numeric($value) || ($value < 1) || ($value >4096)){
+			return;
+		}
+		$this->mapWidth = $value;
+	}
+
+	public function getMapHeight(){
+		return $this->mapHeight;
+	}
+
+	public function setMapHeight($value){
+		if(!is_numeric($value) || ($value < 1) || ($value >4096)){
+			return;
+		}
+		$this->mapHeight = $value;
+	}
+
+	public function getMapZoom(){
+		return $this->mapHeight;
+	}
+
+	public function setMapZoom($value){
+		if(!is_numeric($value) || ($value < 1) || ($value >100)){
+			return;
+		}
+		$this->mapHeight = intval($value);
+	}
 }
 ?>
--- System/WebsiteSupport/JavaScript/GoogleMapsSupport.js
+++ System/WebsiteSupport/JavaScript/GoogleMapsSupport.js
@@ -1,1 +1,26 @@
 //FIXME all the code needed to generate on the fly google-maps here
+
+org.bambuscms.googlemaps = {"maps":{}};
+org.bambuscms.googlemaps.initmaps = function(){
+	for(map_id in org.bambuscms.googlemaps.maps){
+		if (GBrowserIsCompatible()) {
+			org.bambuscms.googlemaps.maps[map_id]['map'] = new GMap2($(map_id));
+			org.bambuscms.googlemaps.maps[map_id]['point'] = new GLatLng(
+				org.bambuscms.googlemaps.maps[map_id]['lat'],
+				org.bambuscms.googlemaps.maps[map_id]['long']
+			);
+			org.bambuscms.googlemaps.maps[map_id]['map'].setCenter(org.bambuscms.googlemaps.maps[map_id]['point'], 13);
+			org.bambuscms.googlemaps.maps[map_id]['map'].addOverlay(org.bambuscms.googlemaps.maps[map_id]['point']);
+		}
+	}
+};
+org.bambuscms.googlemaps.mkMap = function(id, la, lo){
+	org.bambuscms.googlemaps.maps[id] = {'lat':la, 'long': lo};
+};
+org.bambuscms.addToBodyLoad(org.bambuscms.googlemaps.initmaps);
+
+if(GUnload){
+	org.bambuscms.addToBodyUnLoad(GUnload);
+}
+
+
