bc1b0292a95c5e8d6f6469adce6b646ca1c612e5
Lutz Selke <selke@tutech.de>
2010-05-18 16:52:15 +0000

    charset snafu

--- System/Components/HTMLCleaner.lib/HTMLCleaner/Parser.php
+++ System/Components/HTMLCleaner.lib/HTMLCleaner/Parser.php
@@ -14,15 +14,29 @@ class HTMLCleaner_Parser {
 		"br", "hr", "input", "frame", "img", "area", "link", "col", "base", "basefont", "param"
 	);
 
-	public function  __construct($html = null) {
+	public function  __construct($html = null, $wrapHTML = true) {
 		if($html !== null){
-			$this->loadHTML($html);
+			$this->loadHTML($html, $wrapHTML);
 		}
 	}
 
-	public function loadHTML($html){
+	public function loadHTML($html, $wrapHTML = true){
+		$head = '';
+		$foot = '';
+		if($wrapHTML){
+			$head = '<?xml version="1.0" encoding="utf-8"?>'.
+					'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" '.
+					'"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'.
+					'<html lang="de" xmlns="http://www.w3.org/1999/xhtml">'.
+					'<head>'.
+					'<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'.
+					'</head><body>';
+			$foot = '</body></html>';
+		}
+
+
 		$this->domDocument = new DOMDocument('1.0', CHARSET);
-		$this->domDocument->loadHTML($html);
+		$this->domDocument->loadHTML($head.$html.$foot);
 		$this->domDocument->encoding = CHARSET;
 		$this->domDocument->preserveWhiteSpace = false;
 	}
@@ -78,6 +92,7 @@ class HTMLCleaner_Parser {
 		$root = $this->domDocument->documentElement;
 		$this->nodeWalker($root);
 		$body = $this->domDocument->saveXML();
+
 		$body = substr($body, strpos($body, '<body>')+6);
 		$body = substr($body, 0, strripos($body, '</body>'));
 		$body = preg_replace_callback('#<(\w+)([^>]*)\s*/>#s', 'HTMLCleaner_Parser::convCallback' , $body);
