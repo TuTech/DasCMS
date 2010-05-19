c23c35e41b5439ef81e980f6a8fc9f9642bd7d59
Lutz Selke <selke@tutech.de>
2010-05-19 07:36:18 +0000

    added show/hide capabilities to the content activity detector delegate

--- System/Components/ContentSupport.lib/Controller/View/ContentActivityDetectorDelegate.php
+++ System/Components/ContentSupport.lib/Controller/View/ContentActivityDetectorDelegate.php
@@ -6,37 +6,79 @@
  */
 class Controller_View_ContentActivityDetectorDelegate {
     protected $detectInViews = array();
+    protected $showIfViewsContainContent = array();
+    protected $hideIfViewsContainContent = array();
 
-	public function contentViewShouldDisplay(_View_Content_Base $view, Interface_Content $content){
-		$views = $this->detectInViews;
-		$activeViews = VSpore::activeSpores();
-		$contentActiveIn = array();
-		$id = $content->getId();
-		
-		if(count($views) == 0){
-			//default to all views
-			$views = $activeViews;
-		}
-		else{
-			//only active views
-			$views = array_intersect($views, $activeViews);
-		}
-
-		//find views our content is active in
-		foreach ($views as $viewName){
-			$v = VSpore::byName($viewName);
-			if($id == $v->getContent()->getId()){
-				$contentActiveIn[] = 'active_in_view_'.$viewName;
+	private function isActiveIn(array &$views, &$activeViews, $id){
+		$show = false;
+		foreach ($views as $vcc){
+			if(array_key_exists($vcc, $activeViews)){
+				$v = VSpore::byName($vcc);
+				$show = $show || ($id == $v->getContent()->getId());
 			}
 		}
+		return $show;
+	}
 
-		//set css class
-		$contentActiveIn[] = $view->getCustomCSSClass();
-		$view->setCustomCSSClass(implode(' ', $contentActiveIn));
+	public function contentViewShouldDisplay(_View_Content_Base $view, Interface_Content $content){
+		$show = true;
+		$activeViews = VSpore::activeSpores();
+		$id = $content->getId();
+
+		if(count($this->showIfViewsContainContent) > 0){
+			$show = $this->isActiveIn($this->showIfViewsContainContent, $activeViews, $id);
+		}
+
+		if($show && count($this->hideIfViewsContainContent) > 0){
+			$show = !$this->isActiveIn($this->hideIfViewsContainContent, $activeViews, $id);
+		}
+
+		if($show){
+			$views = $this->detectInViews;
+			$contentActiveIn = array();
+
+			if(count($views) == 0){
+				//default to all views
+				$views = $activeViews;
+			}
+			else{
+				//only active views
+				$views = array_intersect($views, $activeViews);
+			}
+
+			//find views our content is active in
+			foreach ($views as $viewName){
+				$v = VSpore::byName($viewName);
+				if($id == $v->getContent()->getId()){
+					$contentActiveIn[] = 'active_in_view_'.$viewName;
+				}
+			}
+
+			//set css class
+			$contentActiveIn[] = $view->getCustomCSSClass();
+			$view->setCustomCSSClass(implode(' ', $contentActiveIn));
+		}
+		return $show;
 	}
 
 	public function  __sleep() {
-		return array('detectInViews');
+		return array('detectInViews', 'showIfViewsContainContent', 'hideIfViewsContainContent');
+	}
+
+	public function getShowIfViewsContainContent(){
+		return $this->showIfViewsContainContent;
+	}
+
+	public function setShowIfViewsContainContent(array $views){
+		$this->showIfViewsContainContent = array_values($views);
+	}
+
+	public function getHideIfViewsContainContent(){
+		return $this->hideIfViewsContainContent;
+	}
+
+	public function setHideIfViewsContainContent(array $views){
+		$this->hideIfViewsContainContent = array_values($views);
 	}
 
 	public function getViewsToDetectIn(){
@@ -44,10 +86,7 @@ class Controller_View_ContentActivityDetectorDelegate {
 	}
 
 	public function setViewsToDetectIn(array $value){
-		$this->detectInViews = array();
-		foreach ($value as $v){
-			$this->detectInViews[] = strval($v);
-		}
+		$this->detectInViews = array_values($value);
 	}
 }
 ?>
