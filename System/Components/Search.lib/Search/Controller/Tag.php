da5ef585e01d7c6b13bcd4e7df5f0a2842ef7e04
Lutz Selke <selke@tutech.de>
2010-09-28 18:19:48 +0200

    filter for tags

--- System/Components/Search.lib/Search/Controller/Tag.php
+++ System/Components/Search.lib/Search/Controller/Tag.php
@@ -3,33 +3,5 @@ class Search_Controller_Tag
 	extends _Search_Controller
 	implements Search_Label_Tag, Search_Label_Global
 {
-	public function filter(){
-
-
-		return;
-	 
-		//resolve tag
-		
-		//get content ids of contents with tag
-	 
-		//remove contents [not] in list
-
-
-		$rules = array(
-			'Require' => &$this->required,
-			'Veto'    => &$this->vetoed
-		);
-		$db = Core::Database();
-		$dba = $db->createQueryForClass($this);
-		foreach ($rules as $filter => $elements){
-			$query = 'filter'.$filter;
-			if($db->hasQueryForClass($query, $this)){
-				$dba = $dba->call($query);
-				foreach ($elements as $criteria){
-					$dba->withParameters($this->searchId, $criteria)->execute();
-				}
-			}
-		}
-	}
 }
 ?>
