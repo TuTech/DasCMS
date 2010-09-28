9a4bb8710bcaeff21bb719422879d25df3a6c9fa
Lutz Selke <selke@tutech.de>
2010-09-28 17:03:23 +0200

    command line conflicts

--- Management/Tools/buildIndex.php
+++ Management/Tools/buildIndex.php
@@ -1,7 +1,7 @@
 <?php
 chdir(dirname(__FILE__));
 require_once '../../System/main.php';
-if(isset($argv[1]) && $argv[1] == '--debug'){
+if(isset($argv[1]) && $argv[1] == 'debug'){
 	define('DEBUG', true);
 }
 CoreUpdate::run();
