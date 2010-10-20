e003c89471b1cf6357ad3346bf5783b5e032148e
Lutz Selke <selke@tutech.de>
2010-10-20 16:59:48 +0200

    skeletons for search app controller and content

---
+++ System/Components/Search.lib/Controller/Application/Search.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * @copyright Lutz Selke/TuTech Innovation GmbH
+ * @author Lutz Selke <selke@tutech.de>
+ * @since 2010-10-20
+ * @license GNU General Public License 3
+ */
+/**
+ * @package Bambus
+ * @subpackage AppController
+ */
+class Controller_Application_Search
+    extends
+        _Controller_Application
+    implements
+        IGlobalUniqueId,
+        ISupportsOpenDialog
+{
+    const GUID = 'org.bambuscms.applications.search';
+
+    /**
+	 * @var CSearch
+     */
+    protected $target = null;
+
+    public function setTarget($target)
+    {
+        try
+        {
+            if(!empty($target))
+            {
+                $this->target = Controller_Content::getInstance()->openContent($target, 'CSearch');
+            }
+        }
+        catch (Exception $e)
+        {
+            $this->target = null;
+        }
+    }
+
+    public function create(array $param)
+    {
+        parent::requirePermission('org.bambuscms.content.csearch.create');
+        $success = false;
+        if(!empty($param['create']))
+        {
+            try
+            {
+                $this->target = CSearch::Create($param['create']);
+                $this->target->changeSearchIndexingStatus(false);
+                $success = true;
+            }
+            catch (Exception $e)
+            {
+                SNotificationCenter::report('warning', 'could_not_create_search');
+            }
+        }
+        return $success;
+    }
+
+    public function save(array $param)
+    {
+        parent::requirePermission('org.bambuscms.content.csearch.change');
+        if($this->target != null)
+        {
+            if(!empty($param['title']))
+            {
+                $this->target->Title = $param['title'];
+            }
+            if(isset($param['subtitle']))
+            {
+                $this->target->SubTitle = $param['subtitle'];
+            }
+			if(isset($param['content_formatter']))
+            {
+                $this->target->setChildContentFormatter($param['content_formatter']);
+            }
+        }
+    }
+
+    public function delete(array $param)
+    {
+        parent::requirePermission('org.bambuscms.content.csearch.delete');
+        if($this->target != null)
+        {
+            $alias = $this->target->Alias;
+            if(Controller_Content::getInstance()->deleteContent($alias))
+            {
+                $this->target = null;
+            }
+        }
+    }
+
+    public function commit()
+    {
+        if($this->target != null)
+        {
+            $this->target->save();
+        }
+    }
+
+    /**
+     * array(Interface_Content|string file, [string mimetype])
+     *
+     * @return array
+     */
+    public function getSideBarTarget()
+    {
+        $ret = array();
+        if($this->target)
+        {
+            $ret = array($this->target);
+        }
+        return $ret;
+    }
+
+    /**
+     * @return string
+     * (non-PHPdoc)
+     * @see System/Component/Interface/IGlobalUniqueId#getClassGUID()
+     */
+    public function getClassGUID()
+    {
+        return self::GUID;
+    }
+
+    /**
+     * opened object
+     * @return string|null
+     */
+    public function getOpenDialogTarget()
+    {
+        return empty($this->target) ? null : $this->target->Alias;
+    }
+
+    /**
+     * returns all data necessary for the open dialog
+     * @return array
+     * @throws XPermissionDeniedException
+     */
+    public function provideOpenDialogData(array $namedParameters)
+    {
+        if(!$this->isPermitted('view'))
+        {
+            throw new XPermissionDeniedException('view');
+        }
+        $IDindex = Controller_Content::getInstance()->contentIndex('CSearch');
+        $items = array();
+        foreach ($IDindex as $alias => $data)
+        {
+        	list($title, $pubdate) = $data;
+        	$items[] = array($title, $alias, 0, strtotime($pubdate));
+        }
+        $data = array(
+            'title' => SLocalization::get('open'),
+            'nrOfItems' => count($items),
+            'iconMap' => array(CSearch::defaultIcon()->asSize(View_UIElement_Icon::LARGE)->getPath()),
+            'smallIconMap' => array(CSearch::defaultIcon()->asSize(View_UIElement_Icon::EXTRA_SMALL)->getPath()),
+            'itemMap' => array('title' => 0, 'alias' => 1, 'icon' => 2, 'pubDate' => 3),//, 'tags' => 4
+            'sortable' => array('title' => 'title', 'pubDate' => 'pubDate'),
+            'items' => $items
+        );
+        return $data;
+    }
+}
+?>
