4787604f6dd882b549c6e65db5e6542da3f58fd6
Lutz Selke <selke@tutech.de>
2010-11-18 10:01:53 +0100

    site name in feed title

--- System/Components/FeedSupport.lib/XML/Atom/Feed.php
+++ System/Components/FeedSupport.lib/XML/Atom/Feed.php
@@ -114,7 +114,7 @@ class XML_Atom_Feed extends _XML_Atom implements Interface_XML_Atom_ToDOMXML
         {
             $o->c__rights = array(XML_Atom_Text::create($copyright));
         }
-        $o->c__title = array(XML_Atom_Text::create($content->getTitle()));
+        $o->c__title = array(XML_Atom_Text::create(Core::Settings()->get('sitename').$content->getTitle()));
         if(strlen($content->getSubTitle()) > 0)
         {
             $o->c__subtitle = array(XML_Atom_Text::create($content->getSubTitle(),'html'));
