254e04102713a63df7b82168ffaf6bb59c8a6954
Lutz Selke <selke@tutech.de>
2010-12-21 11:29:24 +0100

    bespin for templates

--- System/Applications/Templates.bap/edit.php
+++ System/Applications/Templates.bap/edit.php
@@ -12,6 +12,11 @@ if(isset($Tpl) && $Tpl instanceof CTemplate)
     echo new View_UIElement_ContentTitle($Tpl);
     $editor = new View_UIElement_TextEditor($Tpl->RAWContent);
     $editor->disableSpellcheck();
+	if(file_exists('System/External/Bespin')){
+		$editor->addCssClass('bespin');
+		$editor->setCodeAssist(false);
+		$editor->addCustomAttribute("data-bespinoptions", '{ "stealFocus":true, "syntax": "xml", "tabWidth":4 }');
+	}
     echo $editor;   
 }
 ?>
