70718ce9770b3a41f73e01ce42174ba6f8f451d2
Lutz Selke <selke@tutech.de>
2011-11-02 17:33:30 +0100

    continued mvc development

---
+++ System/Components/_ContentMVC.lib/Controller/ContentProxy/DataController.php
@@ -0,0 +1,26 @@
+<?php
+class ContentProxy_DataController
+{
+	protected $proxy, $content;
+
+	public function __construct(ContentProxyController $proxy) {
+		$this->proxy = $proxy;
+		$this->content = $proxy->_content();
+	}
+	
+	/**
+	 * get the tags
+	 */
+	public function data(){
+		return $this->content->getContent();
+	}
+	
+	/**
+	 * set new tags
+	 * @param type $newTags 
+	 */
+	public function setData($newData){
+		throw new Exception("cannot set data through proxy");
+	}
+}
+?>
